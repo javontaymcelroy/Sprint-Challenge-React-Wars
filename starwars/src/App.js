e0160163f46b2948830a9d788c29ce953600666e
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-15 13:49:36 -0600

    Trying to deploy to Netlify.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,10 +35,10 @@ class App extends Component {
       <div className="App">
       <div className="navigation">
         <div className="links">
-          <a href="#">Home</a>
-          <a href="#">Courses</a>
-          <a href="#">Curriculum</a>
-          <a href="#">About Us</a>
+          <a href="http://reactwars.netlify.com">Home</a>
+          <a href="http://reactwars.netlify.com">Courses</a>
+          <a href="http://reactwars.netlify.com">Curriculum</a>
+          <a href="http://reactwars.netlify.com">About Us</a>
         </div>
         <div>
           <button>Enroll</button>
@@ -46,14 +46,14 @@ class App extends Component {
       </div>
       <div className="hero">
         <h1 className="Header">ReactWars: Academy</h1>
-        <img src="https://i.ibb.co/BwXZRqQ/logo.png" className="logo"/>
+        <img src="https://i.ibb.co/BwXZRqQ/logo.png" alt="star_wars" className="logo"/>
         <h1 className="hero-tag">Enroll at ReactWars: Academy today!</h1>
         <p className="hero-undertag">We Test You To The Breaking Point To Make You The Strongest React Dev. Avaliable!</p>
       </div>
       <div className="Alumni-section">
       <h2 className="Alumni-title">Our Alumni</h2>
       <p className="Alumni-tagline"><mark>"ReactWars: Academy has changed my life! I've made new friends, <br/> and learned so much about React! It's WILD!" - Luke S. - Class of 1810 A.D.</mark></p>
-      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/32571842690761.5abecd0ca1cc2.jpg" className="Alumni" />
+      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/32571842690761.5abecd0ca1cc2.jpg" alt="star_wars" className="Alumni" />
       <StarWarsList chars = {this.state.starwarsChars} />
       </div>
       </div>
